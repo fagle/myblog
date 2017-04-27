464786b80d8c99120be6adfa59953db2f1030100
邓团飞 <ifagle@qq.com>
2017-04-27 15:24:25 +0800

    change reg replace for avatar url

--- wp-content/themes/great/functions.php
+++ wp-content/themes/great/functions.php
@@ -152,8 +152,8 @@ function great_widgets_init() {
 add_action( 'widgets_init', 'great_widgets_init' );
 
 function get_ssl_avatar($avatar) {
-   $avatar = preg_replace('/.*\/avatar\/(.*)\?s=([\d]+)&.*/','<img src="https://secure.gravatar.com/avatar/$1?s=$2" class="avatar avatar-$2" height="$2" width="$2">',$avatar);
-   return $avatar;
+	$avatar = preg_replace('/[^\']*avatar\/([^\']*)/','https://secure.gravatar.com/avatar/$1',$avatar);
+	return $avatar;
 }
 add_filter('get_avatar', 'get_ssl_avatar');
 
